d5f4cb0d6683071e1fbfb6502ebe52c45b9c831a
Tennison Chan <tennison.chan+gh@gmail.com>
2021-12-24 10:40:53 -0800

    Add deposit_lending_pool for deposit the eth in lending pool

--- scripts/aave_borrow.py
+++ scripts/aave_borrow.py
@@ -1,6 +1,9 @@
 from scripts.helpers import get_account, is_forked_local
 from scripts.get_weth import deposit_weth
 from brownie import network, config, interface
+from web3 import Web3
+
+AMOUNT = Web3.toWei(0.1, "ether")
 
 
 def aave_borrow():
@@ -23,10 +26,34 @@ def get_lending_pool():
     return lending_pool
 
 
+def approve_erc20(erc20_address, sender, amount, account):
+    print("Approving ERC20 token ...")
+    # ABI
+    # Address
+    erc20_contract = interface.IERC20(erc20_address)
+    tx = erc20_contract.approve(sender, amount, {"from": account})
+    tx.wait(1)
+    print("Approved!")
+    return tx
+
+
+def deposit_lending_pool(lending_pool, address, amount, account):
+    print("Depositing ...")
+    tx = lending_pool.deposit(address, amount, account, 0, {"from": account})
+    tx.wait(1)
+    print("Deposited!")
+    return tx
+
+
 def main():
-    account = get_account
+    print(f"In network {network.show_active()}")
+    account = get_account()
     weth_address = config["networks"][network.show_active()]["weth_token"]
     if is_forked_local(network.show_active()):
         deposit_weth()
     lending_pool = get_lending_pool()
+    # Approve ERC20 tokens
+    approve_erc20(weth_address, lending_pool.address, AMOUNT, account)
     print(lending_pool)
+    # Deposit to lending pool
+    deposit_lending_pool(lending_pool, weth_address, AMOUNT, account)
